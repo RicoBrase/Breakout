819f6b854802adb7fe3e743a79f0b1f841d8a5c2
Rico Brase <kontakt@rico-brase.de>
2015-03-01 15:38:36 +0100

    Added some more stuff

---
+++ Breakout/src/de/rico_brase/Breakout/gui/elements/buttons/WonMenuButton.java
@@ -0,0 +1,26 @@
+package de.rico_brase.Breakout.gui.elements.buttons;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import de.rico_brase.Breakout.gui.elements.GameButton;
+
+public abstract class WonMenuButton extends GameButton{
+
+	public WonMenuButton(String text, int x, int y, int width, int height) {
+		super(text, x, y, width, height);
+	}
+
+	@Override
+	public void renderButton(Graphics2D g) {
+		g.setColor(new Color(170, 200, 0));
+		g.fillRect(this.xPos, this.yPos, this.width, this.height);
+		g.setColor(new Color(0, 0, 0, 150));
+		g.setStroke(new BasicStroke(4F));
+		g.drawRect(this.xPos+2, this.yPos+2, this.width-4, this.height-4);
+	}
+
+	public abstract void onLeftClick();
+
+}
